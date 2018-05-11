a435a9729c465d6a39c62a9df6a6d7e773b6a05a
caimeng <862786917@qq.com>
2018-05-11 16:08:25 +0800

    auth

--- jaqsmds/server/conf.py
+++ jaqsmds/server/conf.py
@@ -15,5 +15,6 @@ AUTH_EXPIRE = int(os.environ.get("AUTH_EXPIRE", 300))
 
 def variables():
     return {name: str(globals()[name]) for name in
-            ["MONGODB_URI", "REDIS_URL", "FRONTEND", "BACKEND", "PROCESS", "LOG_DIR", "LEVEL"]}
+            ["MONGODB_URI", "REDIS_URL", "FRONTEND", "BACKEND", "PROCESS",
+             "LOG_DIR", "LEVEL", "AUTH", "AUTH_EXPIRE"]}
 
